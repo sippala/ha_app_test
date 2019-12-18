5d18f5cf220e960ba367064675952b69993ebea2
Shankar Ippala <ishan753@gmail.com>
2019-12-18 10:38:37 -0700

    Update alb.tf

--- alb.tf
+++ alb.tf
@@ -5,7 +5,7 @@ resource "aws_security_group" "alb_sg" {
 
   ingress {
     from_port   = 80
-    to_port     = 80
+    to_port     = 8080
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
