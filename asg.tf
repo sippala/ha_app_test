6fc4b3d4ebfd4a12b116e02ca01672c171b6ce92
Shankar Ippala <ishan753@gmail.com>
2019-12-18 11:12:56 -0700

    Update asg.tf

--- asg.tf
+++ asg.tf
@@ -3,6 +3,33 @@ data "template_file" "user_data" {
   vars { }
 }
 
+/*
+resource "aws_security_group" "asg-sg" {
+  name   = "my-test-sg"
+  vpc_id = "${var.vpc_id}"
+}
+
+# Ingress Security Port 22
+resource "aws_security_group_rule" "http_inbound_access" {
+  from_port         = 80
+  protocol          = "tcp"
+  security_group_id = "${aws_security_group.asg-sg.id}"
+  to_port           = 80
+  type              = "ingress"
+  cidr_blocks       = ["0.0.0.0/0"]
+}
+
+# All OutBound Access
+resource "aws_security_group_rule" "all_outbound_access" {
+  from_port         = 0
+  protocol          = "-1"
+  security_group_id = "${aws_security_group.asg-sg.id}"
+  to_port           = 0
+  type              = "egress"
+  cidr_blocks       = ["0.0.0.0/0"]
+}
+*/
+  
 resource "aws_launch_configuration" "web-servers" {
   name_prefix      = "web-server"
   image_id         = "ami-09479453c5cde9639"
@@ -14,17 +41,15 @@ resource "aws_launch_configuration" "web-servers" {
   
   lifecycle        { create_before_destroy = true }
 
-/*
-  user_data = <<USER_DATA
-#!/bin/bash
-yum update
-yum install httpd -y 
-service httpd start
-chkconfig httpd on
-cd /var/www/html
-echo "<html><h1>This is WebServer</h1></html>" > index.html
- USER_DATA
-*/
+#  user_data = <<USER_DATA
+##!/bin/bash
+#yum update
+#yum install httpd -y 
+#service httpd start
+#chkconfig httpd on
+#cd /var/www/html
+#echo "<html><h1>This is WebServer</h1></html>" > index.html
+ #USER_DATA
 
 }
 
