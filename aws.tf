0625d1efee2ce7f86008403d03e443aa85813741
Shankar Ippala <ishan753@gmail.com>
2019-12-18 10:23:04 -0700

    Create aws.tf

---
+++ aws.tf
@@ -0,0 +1,14 @@
+terraform {
+ required_version = ">= 0.11.11"
+ backend "s3" {
+ encrypt = true
+   region = "us-west-2"
+ }
+}
+
+provider "aws" {
+  region = "us-west-2"
+  assume_role {
+    role_arn= "${local.this_role_arn}"
+  }
+}
